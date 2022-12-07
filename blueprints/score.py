59e8df024cbf3c6b1deb58ae3586e06af89ec349
hanjuTsai <rita20337@gmail.com>
2022-12-07 15:03:59 -0500

    add score board api

--- blueprints/score.py
+++ blueprints/score.py
@@ -1,7 +1,7 @@
 import requests
 from flask import Blueprint, request, Response, render_template, url_for
 from utils.exts import db
-from utils.model import Question, QuizQuestion, Quiz, UserAnswer
+from utils.model import UserQuiz
 import json
 
 bp = Blueprint("score", __name__, url_prefix="/score")
@@ -10,8 +10,23 @@ bp = Blueprint("score", __name__, url_prefix="/score")
 @bp.route("/", methods=["GET"])
 def all_score():
   args = request.args
-  recent_entry_num = args.get('recent_entry_num')
-  resp = {}
-
+  recent_entry_num = args.get('recent_entry_num', None)
+  resp = {"users":[]}
   
+  query = db.session.query(UserQuiz) \
+    .order_by(UserQuiz.created_at.desc()) \
+  
+  if recent_entry_num:
+    query = query.limit(int(recent_entry_num))
+
+  query = query.order_by(UserQuiz.score.desc())
+
+  for user_quiz in query:
+    resp["users"].append({
+      "user_id": user_quiz.user_id,
+      "quiz_id": user_quiz.quiz_id,
+      "score": user_quiz.score,
+      "quiz_taken_at": str(user_quiz.created_at),
+    })
+
   return Response(json.dumps(resp), status=200)
--- utils/model.py
+++ utils/model.py
@@ -1,60 +1,15 @@
 from utils.exts import db
 import datetime
 
-class Quiz(db.Model):
-    __tablename__ = "quiz"
-    quiz_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    difficulty = db.Column(db.Integer)
+class UserQuiz(db.Model):
+  __tablename__ = "user_quiz"
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  user_id = db.Column(db.Integer)
+  quiz_id = db.Column(db.Integer)
+  score = db.Column(db.Float)
+  created_at = db.Column(db.DateTime, default=datetime.datetime.now)
 
-    def __init__(self, quiz_id, difficulty):
-        self.quiz_id = quiz_id
-        self.difficulty = difficulty
-
-class Question(db.Model):
-    __tablename__ = "question"
-    question_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    q_type = db.Column(db.Integer)
-    c_type = db.Column(db.Integer)
-    description = db.Column(db.String(1024))
-    content = db.Column(db.String(1024))
-    answer = db.Column(db.String(256))
-    difficulty = db.Column(db.Integer)
-    image_url = db.Column(db.String(1024))
-
-    def __init__(self, question_id, q_type, c_type, description, content, answer, difficulty, image_url):
-        self.question_id = question_id
-        self.q_type = q_type
-        self.c_type = c_type
-        self.description = description
-        self.content = content
-        self.answer = answer
-        self.difficulty = difficulty
-        self.image_url = image_url
-
-
-class QuizQuestion(db.Model):
-    __tablename__ = "quiz_question"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    quiz_id = db.Column(db.Integer)
-    question_id = db.Column(db.Integer)
-
-    def __init__(self, quiz_question_id, quiz_id, question_id):
-        self.id = quiz_question_id
-        self.quiz_id = quiz_id
-        self.question_id = question_id
-
-
-class UserAnswer(db.Model):
-    __tablename__ = "user_answer"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer)
-    quiz_id = db.Column(db.Integer)  # Different quiz have same question
-    question_id = db.Column(db.Integer)
-    user_answer = db.Column(db.String(256))
-    created_at = db.Column(db.DateTime, default=datetime.datetime.now) # the time when user took the quiz
-
-    def __init__(self, user_id, quiz_id, question_id, user_answer):
-        self.user_id = user_id
-        self.quiz_id = quiz_id
-        self.question_id = question_id
-        self.user_answer = user_answer
+  def __init__(self, user_id: int, quiz_id: int, score: float):
+    self.user_id = user_id
+    self.quiz_id = quiz_id
+    self.score = score
